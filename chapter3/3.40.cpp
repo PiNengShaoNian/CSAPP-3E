e2940d96a8d5f94ce9d574459678c312acfd6985
quguiyou <quguiyou1076@qq.com>
2022-01-01 10:50:33 +0800

    3.40

---
+++ chapter3/3.40.cpp
@@ -0,0 +1,9 @@
+void fix_set_diag_opt(fix_matrix A, int val) {
+    int *Abase = &A[0][0];
+    long i = 0;
+    long iend = N * (N + 1);
+    do {
+        Abase[i] = val;
+        i += (N + 1);
+    } while(i != iend)
+}
