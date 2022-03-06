e11b9336dfd6ddf905980856f0c08b2fcf5eda0c
瞿贵友 <quguiyou1076@qq.com>
2022-03-06 17:22:04 +0800

    6-7至6-8

--- chapter6/6.7.c
+++ chapter6/6.7.c
@@ -1,9 +1,9 @@
 int sumarray3d(int a[N][N][N]) {
     int i, j, k, sum = 0;
 
-    for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < N; ++j) {
-            for(int k = 0; k < N; ++k) {
+    for(i = 0; i < N; ++i) {
+        for(j = 0; j < N; ++j) {
+            for(k = 0; k < N; ++k) {
                 sum += a[i][j][k];
             }
         }
