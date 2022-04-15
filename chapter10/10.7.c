07163e46f9ddbb19190e5a697a1a0de18332b5bd
瞿贵友 <quguiyou1076@qq.com>
2022-04-15 20:16:39 +0800

    10.2-10.7

---
+++ chapter10/10.7.c
@@ -0,0 +1,13 @@
+#include "csapp.h"
+
+int main(int argc, char **argv) 
+{
+    int n;
+    rio_t rio;
+    char buf[MAXBUF];
+
+    Rio_readinitb(&rio, STDIN_FILENO);
+    while((n = Rio_readnb(&rio, buf, MAXBUF)) != 0) 
+	    Rio_writen(STDOUT_FILENO, buf, n);
+    exit(0);
+}
