c5f82fcab6f620ef829df08bd19cca649a3244c6
quguiyou <quguiyou1076@qq.com>
2022-04-29 22:15:07 +0800

    12.38

---
+++ chapter12/12.38.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include "csapp.h"
+#include "sbuf.h"
+
+#define SBUFSIZE 4
+#define INIT_THREAD_N 1
+#define THREAD_LIMIT 4096
+
+static int nthreads;
+static sbuf_t sbuf; /* Shared buffer of conncted descriptors */
+
+// thread info
+typedef struct {
+    pthread_t tid;
+    sem_t mutex;
+}
+
+static ithread threads[THREAD_LIMIT];
+
+// init work
+void init(void);
+// function for create server thread
+void *serve_thread(void *vargp);
+
+/*
+ * creating thread that adjust total thread number according to sbuf situation
+ * if sbuf is full, double threads
+ * if sbuf is empty, half threads 
+ */
+
+// from start to end, create (end - start) new server threads
+void create_threads(int start, int end);
+
+int main(int argc, char **argv) {
+    int i, listenfd, connfd;
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+    pthread_t tid;
+
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        fprintf(stderr, "use default port 5000\n");
+        listenfd = Open_listenfd("5000");
+    }
+    else {
+        listenfd = Open_listenfd(argv[1]);
+    }
+
+    init();
+
+    while(1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+        sbuf_insert(&sbuf, connfd);
+    }
+}
+
+void init(void) {
+    nthreads = INIT_THREAD_N;
+    sbuf_init(&sbuf, SBUFSIZE);
+
+    // create initial server threads
+    create_threads(0, nthreads);
+}
+
+void *serve_thread(void *vargp) {
+    int idx = *(int *)vargp;
+    Free(vargp);
+
+    while(1) {
+        // get lock first
+        // thread can't be kill now
+        P(&(threads[idx].mutex));
+        int connfd = subf_remove(&sbuf);
+        doit(connfd);
+        Close(connfd);
+
+        // service ends and release lock
+        // so thread can be kill at this time
+        V(&(threads[idx].mutex));
+    }
+}
+
+void create_threads(int start, int end) {
+    int i;
+    for(i = start; i < end; i++) {
+        // init mutex for every new thread
+        Sem_init(&threads[i].mutex, 0, 1);
+        // create thread
+        int *arg = (int *)Malloc(sizeof(int));
+        *arg = i;
+        Pthread_create(&threads[i].tid, NULL, serve_thread, arg);
+    }
+}
+
+void *adjust_threads(void *vargp) {
+    sbuf_t *sp = &sbuf;
+
+    while(1) {
+        // if sbuf is full, double threads
+        if(sbuf_full(sp)) {
+            if(nthreads == THREAD_LIMIT) {
+                fprintf(stderr, "too many threads, can't double\n");
+                continue;
+            }
+
+            // double n
+            int dn = nthreads * 2;
+            create_threads(nthreads, dn);
+            nthreads = dn;
+            continue;
+        }
+
+        // half threads
+        if(sbuf_empty(sp)) {
+            if(nthreads == 1) {
+                continue;
+            }
+
+            // half n
+            int hn = nthreads / 2;
+            /*
+             * all server thread are divide to 2 parts
+             * keep [0, hn] running
+             * kill [hn, nthreads] threads
+             * 
+             * if you want to kill a thread, you must get the lock before it
+             * so you won't kill a thread which is offering service.
+             */
+
+             int i;
+             for(i = hn; i < nthreads; i++) {
+                 P(&(threads[i].mutex));
+                 Pthread_cancel(threads[i].tid);
+                 V(&(threads[i].mutex));
+             }
+
+             nthreads = hn;
+             continue;
+        }
+    }
+}
